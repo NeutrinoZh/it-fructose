2545542e9beafe32fdf14fa270b8f33085c1c6ce
NeutrinoZh <neutrinozh@gmail.com>
2024-01-25 23:15:36 +0200

    user block

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -8,11 +8,15 @@ import styles from "./page.module.scss";
 export default function Page(): JSX.Element {
   return (
     <main className={styles.main}>
-      <div className={classnames(styles.block, styles.userBlock)}>
-        <img src={avatar.src}></img>
+      <div className={classnames(styles.block, styles.titleBlock)}>
+        <div className={styles.user}>
+          <img src={avatar.src}></img>
+          <p className={styles.name}>{data.user.name}</p>
+          <p className={styles.job}>{data.user.job}</p>
+        </div>
         <p>{data.page.title}</p>
       </div>
-      <div className={classnames(styles.block, styles.titleBlock)}>
+      <div className={classnames(styles.block)}>
 
       </div>
     </main>
