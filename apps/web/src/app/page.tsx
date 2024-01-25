da5cca105e9871e67739384371cc873b125aa119
NeutrinoZh <neutrinozh@gmail.com>
2024-01-25 22:37:41 +0200

    update color pallete. user block.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,16 +1,19 @@
-import data from "../data/data";
-import styles from "./page.module.scss";
 import React from "react";
 import classnames from 'classnames';
+
+import data from "../data/data";
+import avatar from "../../public/avatar.jpg"
+import styles from "./page.module.scss";
 
 export default function Page(): JSX.Element {
   return (
     <main className={styles.main}>
       <div className={classnames(styles.block, styles.userBlock)}>
-
+        <img src={avatar.src}></img>
+        <p>{data.page.title}</p>
       </div>
       <div className={classnames(styles.block, styles.titleBlock)}>
-        <p>{data.page.title}</p>
+
       </div>
     </main>
   );
