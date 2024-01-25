18a1b49c5f9acec1e3256fbd3f1da942aeb98545
NeutrinoZh <neutrinozh@gmail.com>
2024-01-25 20:04:29 +0200

    initial turborepo template

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,12 @@
+import "./globals.css";
+import styles from "./page.module.css";
+
+import React from "react";
+
+export default function Page(): JSX.Element {
+  return (
+    <main>
+      <h1>Hello, world!</h1>
+    </main>
+  );
+}
