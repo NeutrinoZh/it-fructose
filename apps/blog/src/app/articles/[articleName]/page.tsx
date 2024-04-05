45685bab6a6fa51c71fff3b814412901d7ba25f9
NeutrinoZh <neutrinozh@gmail.com>
2024-04-05 08:30:44 +0300

    data fetching error handling

--- apps/blog/src/app/articles/[articleName]/page.tsx
+++ apps/blog/src/app/articles/[articleName]/page.tsx
@@ -2,18 +2,23 @@ import Markdown from "react-markdown";
 import api from "../../../api/api";
 import styles from "./article.module.scss"
 import remarkGfm from "remark-gfm";
+import notFound from "../../not-found";
 
 export default async function Article({ params }: { params: { articleName: string } }) {
-  const content = await api.getArticleByName(params.articleName);
+  try {
+    const content = await api.getArticleByName(params.articleName);
 
-  return (
-    <article className={styles.article}>
-      <Markdown
-        remarkPlugins={[remarkGfm]}
-        className={styles.markdown}
-      >
-        {content}
-      </Markdown>
-    </article>
-  );
+    return (
+      <article className={styles.article}>
+        <Markdown
+          remarkPlugins={[remarkGfm]}
+          className={styles.markdown}
+        >
+          {content}
+        </Markdown>
+      </article>
+    );
+  } catch (err) {
+    return notFound()
+  }
 }
