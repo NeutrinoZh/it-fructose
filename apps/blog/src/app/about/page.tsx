ba5cc99f77812a9062028ff7c2d159516d9953f6
NeutrinoZh <neutrinozh@gmail.com>
2024-07-20 08:44:25 +0300

    update portfolio

--- apps/blog/src/app/about/page.tsx
+++ apps/blog/src/app/about/page.tsx
@@ -22,6 +22,7 @@ export default async function About() {
     const history = await api.getAbout();
     const listOfProjects = chunkArray(await api.getPortfolioProjects(), 2);
 
+
     return (
         <div className={styles.about}>
             <div className={styles.user}>
